777f799982e98ef5cf3b0bab50b4bff829aed4c5
nazim <djeblounnazim2@gmail.com>
2026-03-22 19:34:45 +0100

    Add optimizePackageImports and poweredByHeader: false

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'radix-ui', 'clsx', 'cva', '@radix-ui/react-dialog', '@radix-ui/react-scroll-area', '@radix-ui/react-separator', '@radix-ui/react-tabs', '@radix-ui/react-avatar', '@radix-ui/react-switch', '@radix-ui/react-slot'],
+  },
   images: {
     remotePatterns: [
       {
